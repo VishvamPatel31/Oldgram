27f73752a4d35532c6a72eed8ecd900d8ba189d9
VishvamPatel31 <96796901+VishvamPatel31@users.noreply.github.com>
2025-03-18 11:45:45 -0400

    added js script for dynamic rendering of posts

--- scripts/script.js
+++ scripts/script.js
@@ -27,3 +27,47 @@ const posts = [
         likes: 152
     }
 ]
+
+let html = ' '
+
+let container = document.getElementById("main")
+
+function renderposts(){
+    for(let i=0; i<posts.length; i++){
+        html += `
+            <section class="section1">
+                    <div class="user">
+                        <img class="profile" src="${posts[i].avatar}">
+                        <div class="userinfo">
+                            <p class="name">${posts[i].name}</p>
+                            <p class="location">${posts[i].location}</p>
+                        </div>
+                    </div>
+                </section>     
+
+                    <img class="post" src="${posts[i].post}">
+
+                <section class="section2">
+                        <div class="btns">
+                            <button ><img class="likebtn btn" src="images/icon-heart.png" ></button>
+                            <button ><img class="commentbtn btn" src="images/icon-comment.png"> </button>
+                            <button ><img class="sharebtn btn" src="images/icon-dm.png"> </button>
+                        </div>
+                        <p class="likes">${posts[i].likes} likes</p>
+
+                        <div class="postinfo">
+                            <p class="username"> ${posts[i].username}</p>
+                            <p class="caption"> ${posts[i].comment}</p>
+                        </div>
+                </section>
+            </div>
+
+        `
+        container.innerHTML += html
+        html = ''
+    }
+    
+}
+
+
+renderposts()
